e79e1a35e19dc747946dd28326185ab74b645157
AlexBarbosa169 <alexsrb169@hotmail.com>
2017-11-09 09:13:03 -0200

    VersãoComRelacionamentos

--- bancoclinicamodelo.sql
+++ bancoclinicamodelo.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 07-Nov-2017 às 13:18
--- Versão do servidor: 5.7.14
--- PHP Version: 7.0.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `bancoclinicamodelo`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `consulta`
---
 
 CREATE TABLE `consulta` (
   `codConsulta` int(11) NOT NULL,
@@ -34,22 +15,16 @@ CREATE TABLE `consulta` (
   `descricaoConsulta` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `exame`
---
-
 CREATE TABLE `exame` (
   `tipoExame` int(11) NOT NULL,
   `descricaoExame` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `medico`
---
+CREATE TABLE `medicamento` (
+  `cod_medicamento` int(11) NOT NULL,
+  `Nome` text NOT NULL,
+  `Descricao` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `medico` (
   `CRM` int(10) NOT NULL,
@@ -58,12 +33,6 @@ CREATE TABLE `medico` (
   `DataNasc` date NOT NULL,
   `Especialidade` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `paciente`
---
 
 CREATE TABLE `paciente` (
   `CPF` int(11) NOT NULL,
@@ -75,33 +44,52 @@ CREATE TABLE `paciente` (
   `CodPaciente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
+CREATE TABLE `prescricao` (
+  `codPrescricao` int(11) NOT NULL,
+  `codConsulta` int(11) NOT NULL,
+  `codMedicamento` int(11) DEFAULT NULL,
+  `codExame` int(11) DEFAULT NULL,
+  `Comentario` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for table `consulta`
---
+
 ALTER TABLE `consulta`
-  ADD PRIMARY KEY (`codConsulta`);
+  ADD PRIMARY KEY (`codConsulta`),
+  ADD UNIQUE KEY `codPaciente` (`codPaciente`),
+  ADD UNIQUE KEY `crmMedico` (`crmMedico`);
 
---
--- Indexes for table `exame`
---
 ALTER TABLE `exame`
   ADD PRIMARY KEY (`tipoExame`);
 
---
--- Indexes for table `medico`
---
+ALTER TABLE `medicamento`
+  ADD PRIMARY KEY (`cod_medicamento`);
+
 ALTER TABLE `medico`
   ADD PRIMARY KEY (`CRM`);
 
---
--- Indexes for table `paciente`
---
 ALTER TABLE `paciente`
   ADD PRIMARY KEY (`CodPaciente`);
+
+ALTER TABLE `prescricao`
+  ADD PRIMARY KEY (`codPrescricao`),
+  ADD UNIQUE KEY `codConsulta` (`codConsulta`),
+  ADD UNIQUE KEY `codMedicamento` (`codMedicamento`),
+  ADD UNIQUE KEY `codExame` (`codExame`);
+
+
+ALTER TABLE `medicamento`
+  MODIFY `cod_medicamento` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `prescricao`
+  MODIFY `codPrescricao` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `consulta`
+  ADD CONSTRAINT `FK_MEDICO` FOREIGN KEY (`crmMedico`) REFERENCES `medico` (`CRM`),
+  ADD CONSTRAINT `FK_PACIENTE` FOREIGN KEY (`codPaciente`) REFERENCES `paciente` (`CodPaciente`);
+
+ALTER TABLE `prescricao`
+  ADD CONSTRAINT `FK_CONSULTA` FOREIGN KEY (`codConsulta`) REFERENCES `consulta` (`codConsulta`),
+  ADD CONSTRAINT `FK_EXAME` FOREIGN KEY (`codExame`) REFERENCES `exame` (`tipoExame`),
+  ADD CONSTRAINT `FK_MEDICAMENTO` FOREIGN KEY (`codMedicamento`) REFERENCES `medicamento` (`cod_medicamento`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
